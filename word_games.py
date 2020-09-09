e9ad1061805f5e50b806a0a4754a273c9e1c7685
Adam <adam.mhatre@menloschool.org>
2020-09-09 15:13:48 -0700

    problem 1 solved

---
+++ word_games.py
@@ -0,0 +1,14 @@
+def point_function(word):
+	dictpoints = {"a" : 1, "b" : 3, "c": 3, "d" : 2,
+					"e" : 1, "f" : 4, "g" : 2, "h" : 4, "i" : 1,
+					"j" : 8, "k" : 5, "l" : 1, "m" : 3, "n" : 1,
+					"o" : 1, "p" : 3, "q" : 10, "r" : 1, "s" : 1,
+					"t" : 1, "u" : 1, "v" : 4, "w" : 4, "x" : 8,
+					"y" : 4, "z" : 10}
+	sum = 0
+	for x in word:
+		sum = sum + dictpoints[x]
+
+	return sum
+
+print (point_function("bad"))
